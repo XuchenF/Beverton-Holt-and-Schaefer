fcacf1396edff7b8495f4bb45c5a1cbdf84d2251
XuchenF <131358300+XuchenF@users.noreply.github.com>
2024-04-01 17:18:52 +0800

    Beverton-holt driven produciton model.R

--- Beverton-holt driven produciton model.R
+++ Beverton-holt driven produciton model.R
@@ -1,16 +1,48 @@
+k<-100000
+M=1.19
+h<-0.36
+hh<-0.99
+
+b<-(h-0.2)/(0.2*k-0.2*h*k)
+a<-(k*(1-exp(-1*M))*(1+b*k))/k
+
+bb<-(hh-0.2)/(0.2*k-0.2*hh*k)
+aa<-(k*(1-exp(-1*M))*(1+bb*k))/k
+
 #import a col from the excel spreedsheet into the R
 B<-read.csv("Biomass-simulation.csv")
-B<-B[,1]
 #Using Beverton-Holt driven production model to calculate corresponding ideal catch
 ##All the parameters are based on the Yellow Sea anchovy information
-C<-vector( "numeric" , length(B)-1 )
-for(i in 1:length(C)){
-if (B[i+1] > 1.566*B[i]/(1+0.0000125*B[i])){
-F[i]=(-1)*log((B[i+1]-(1.566*B[i]/(1+0.0000125*B[i])))/B[i])-1.19
-C[i]=(F[i]/(F[i]+1.19))*B[i]*(1-exp(-(F[i]+1.19)))
+#h=0.36
+C<-data.frame(matrix(ncol=6, nrow=50))
+F<-data.frame(matrix(ncol=6, nrow=50))
+for(j in 1:6){
+for(i in 1:50){
+if (B[i+1,j] > a*B[i,j]/(1+b*B[i,j])){
+F[i,j]=(-1)*log((B[i+1,j]-(a*B[i,j]/(1+b*B[i,j])))/B[i,j])-M
+C[i,j]=(F[i,j]/(F[i,j]+M))*B[i,j]*(1-exp(-(F[i,j]+M)))
 }
 else
 {
-C[i]=((log(10*B[i])-1.19)/log(10*B[i]))*B[i]+(1.566*B[i]/(1+0.0000125*B[i]))-B[i+1]
+C[i,j]=((log(10*B[i,j])-M)/log(10*B[i,j]))*B[i,j]+(a*B[i,j]/(1+b*B[i,j]))-B[i+1,j]
 }
 }
+}
+
+#hh=0.99
+C<-data.frame(matrix(ncol=6, nrow=50))
+F<-data.frame(matrix(ncol=6, nrow=50))
+for(j in 1:6){
+for(i in 1:50){
+if (B[i+1,j] > aa*B[i,j]/(1+bb*B[i,j])){
+F[i,j]=(-1)*log((B[i+1,j]-(aa*B[i,j]/(1+bb*B[i,j])))/B[i,j])-M
+C[i,j]=(F[i,j]/(F[i,j]+M))*B[i,j]*(1-exp(-(F[i,j]+M)))
+}
+else
+{
+C[i,j]=((log(10*B[i,j])-M)/log(10*B[i,j]))*B[i,j]+(aa*B[i,j]/(1+bb*B[i,j]))-B[i+1,j]
+}
+}
+}
+
+

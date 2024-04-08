3690284017660cd3b413e2d91130a0b503497a48
XuchenF <131358300+XuchenF@users.noreply.github.com>
2024-04-08 10:51:08 +0800

    Beverton-holt driven produciton model.R

--- Beverton-holt driven produciton model.R
+++ Beverton-holt driven produciton model.R
@@ -25,21 +25,26 @@ C[i,j]=((log(10*B[i,j])-M)/log(10*B[i,j]))*B[i,j]+(a*B[i,j]/(1+b*B[i,j]))-B[i+1,
 }
 }
 
-B<-data.frame(matrix(ncol=1, nrow=34))
-F<-data.frame(matrix(ncol=1, nrow=33))
-Binit<-c((max(C[,2])/1.76+4*max(C[,2])/0.98)/2*0.6, Binit<-(max(C[,2])/1.76+4*max(C[,2])/0.98)/2*0.8)
+
+k<-(max(C[,2])/1.76+4*max(C[,2])/0.98)/2
+M=0.9
+h<-0.66
+b<-(h-0.2)/(0.2*k-0.2*h*k)
+a<-(k*(1-exp(-1*M))*(1+b*k))/k
+B<-data.frame(matrix(ncol=1, length(C[,1])))
+F<-data.frame(matrix(ncol=1, length(C[,1])))
+Binit<-c(k*0.6,k*0.8)
+B[1,1]<-(Binit[1]+Binit[2])/2
+
 for(i in 1:length(C[,1])){
-if(i-1<1){
-y=function(x){x/(x+0.9)*Binit*(1-exp(-1*(x+0.9)))-C[i,2]}
+  if(C[i,2]>B[i,1]){
+y=function(x){x/(x+M)*B[i,1]*(1-exp(-1*(x+M)))-C[i,2]}
 so<-uniroot(y, interval = c(0, 17))
 F[i,1]<-so$root
-B[i,1]<-a*Binit/(1+b*Binit)+Binit*exp(-1*(F[i,1]+0.9))
+B[i+1,1]<-a*B[i,1]/(1+b*B[i,1])+B[i,1]*exp(-1*(F[i,1]+M))
 }
-else
-{
-y=function(x){x/(x+0.9)*B[i-1,1]*(1-exp(-1*(x+0.9)))-C[i,2]}
-so<-uniroot(y, interval = c(0, 17))
-F[i,1]<-so$root
-B[i,1]<-a*Binit/(1+b*B[i-1,1])+B[i-1,1]*exp(-1*(F[i,1]+0.9))
-}
+  else
+    F[i,1]<-log(B[i,1]*10)-M
+  B[i+1,1]<-F[i,1]/(F[i,1]+M)*B[i,1]+a*B[i,1]/(1+b*B[i,1])-C[i,2]
+  }
 }

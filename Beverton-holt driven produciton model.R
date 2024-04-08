beb0f44b788f5e559260e2afe46e0363f03155ba
XuchenF <131358300+XuchenF@users.noreply.github.com>
2024-04-08 09:58:25 +0800

    Beverton-holt driven produciton model.R

--- Beverton-holt driven produciton model.R
+++ Beverton-holt driven produciton model.R
@@ -24,3 +24,21 @@ C[i,j]=((log(10*B[i,j])-M)/log(10*B[i,j]))*B[i,j]+(a*B[i,j]/(1+b*B[i,j]))-B[i+1,
 }
 }
 }
+
+
+Binit<-c((max(C[,2])/1.76+4*max(C[,2])/0.98)/2*0.6, Binit<-(max(C[,2])/1.76+4*max(C[,2])/0.98)/2*0.8)
+for(i in 1:length(C[,1])){
+if(i-1<1){
+y=function(x){x/(x+0.9)*Binit*(1-exp(-1*(x+0.9)))-C[i,2]}
+so<-uniroot(y, interval = c(0, 17))
+F[i,1]<-so$root
+B[i,1]<-a*Binit/(1+b*Binit)+Binit*exp(-1*(F[i,1]+0.9))
+}
+else
+{
+y=function(x){x/(x+0.9)*B[i-1,1]*(1-exp(-1*(x+0.9)))-C[i,2]}
+so<-uniroot(y, interval = c(0, 17))
+F[i,1]<-so$root
+B[i,1]<-a*Binit/(1+b*B[i-1,1])+B[i-1,1]*exp(-1*(F[i,1]+0.9))
+}
+}
